f1295488649d5c014627c7a9f96641ce6b258a6c
Anson Kindred <anson@anson-desktop.(none)>
2012-09-26 15:11:54 -0400

    Initial commit to new repo

---
+++ src/GLRenderer.java
@@ -0,0 +1,181 @@
+import java.util.ArrayList;
+import javax.media.opengl.GL2;
+import javax.media.opengl.GLAutoDrawable;
+import javax.media.opengl.GLCapabilities;
+import javax.media.opengl.GLEventListener;
+import javax.media.opengl.GLProfile;
+import javax.media.opengl.awt.GLCanvas;
+import javax.media.opengl.glu.GLU;
+
+import poomonkeys.common.Drawable;
+import poomonkeys.common.Renderer;
+
+import com.jogamp.opengl.util.FPSAnimator;
+
+public class GLRenderer extends GLCanvas implements GLEventListener, Renderer
+{
+
+	private static final long serialVersionUID = -8513201172428486833L;
+	public float viewWidth, viewHeight;
+	public float screenWidth, screenHeight;
+
+	private static GLRenderer instance = null;
+
+	public long timeSinceLastDraw;
+	private long lastDrawTime;
+
+	private FPSAnimator animator;
+
+	private ArrayList<Drawable> drawables = new ArrayList<Drawable>();
+	private boolean didInit = false;
+
+	public static GLRenderer getInstance()
+	{
+		if (instance == null)
+			instance = new GLRenderer();
+		return instance;
+	}
+
+	public GLRenderer()
+	{
+		// setup OpenGL Version 2
+		super(new GLCapabilities(GLProfile.get(GLProfile.GL2)));
+
+		this.addGLEventListener(this);
+		this.setSize(1800, 1000);
+
+		animator = new FPSAnimator(this, 60);
+		animator.start();
+	}
+
+	public void registerDrawable(Drawable d)
+	{
+		drawables.add(d);
+	}
+
+	public void unregisterDrawable(Drawable d)
+	{
+		drawables.remove(d);
+	}
+
+	public void init(GLAutoDrawable d)
+	{
+		final GL2 gl = d.getGL().getGL2();
+		gl.glClearColor(0f, 0f, 0f, 1f);
+
+		gl.glBlendFunc(GL2.GL_SRC_ALPHA, GL2.GL_ONE_MINUS_SRC_ALPHA);
+		gl.glEnable(GL2.GL_BLEND);
+	}
+
+	public void display(GLAutoDrawable d)
+	{
+		final GL2 gl = d.getGL().getGL2();
+		timeSinceLastDraw = System.currentTimeMillis() - lastDrawTime;
+		lastDrawTime = System.currentTimeMillis();
+		gl.glClear(GL2.GL_COLOR_BUFFER_BIT | GL2.GL_DEPTH_BUFFER_BIT);
+		gl.glActiveTexture(GL2.GL_TEXTURE0);
+
+		gl.glLoadIdentity();
+
+		for (int i = 0; i < drawables.size(); i++)
+		{
+			if (!drawables.get(i).didInit)
+				drawables.get(i).init(viewWidth, viewHeight);
+			drawables.get(i).update(this);
+			this.drawDrawable(drawables.get(i), gl);
+		}
+	}
+
+	public void drawDrawable(Drawable thing, GL2 gl)
+	{
+		gl.glPushMatrix();
+
+		gl.glTranslatef(thing.x, thing.y, 0);
+
+		if (thing.rotation != 0)
+		{
+			gl.glRotatef(thing.rotation, 0, 0, 1);
+		}
+		if (thing.baseGeometry != null)
+		{
+			this.render(gl, thing.drawMode, thing);
+		}
+
+		for (int i = 0; i < thing.drawables.size(); i++)
+		{
+			thing.drawables.get(i).update(this);
+			this.drawDrawable(thing.drawables.get(i), gl);
+		}
+
+		gl.glPopMatrix();
+	}
+
+	public void reshape(GLAutoDrawable d, int x, int y, int width, int height)
+	{
+		final GL2 gl = d.getGL().getGL2();
+		gl.glViewport(0, 0, width, height);
+		float ratio = (float) height / width;
+
+		screenWidth = width;
+		screenHeight = height;
+		viewWidth = 100;
+		viewHeight = viewWidth * ratio;
+
+		gl.glMatrixMode(GL2.GL_PROJECTION);
+		gl.glLoadIdentity();
+		(new GLU()).gluOrtho2D(-viewWidth/2, viewWidth/2, -viewHeight/2, viewHeight/2);
+
+		gl.glMatrixMode(GL2.GL_MODELVIEW);
+		gl.glLoadIdentity();
+
+		if (!didInit)
+		{
+			didInit = true;
+			for (int i = 0; i < drawables.size(); i++)
+				drawables.get(i).init(viewWidth, viewHeight);
+		} else
+		{
+			for (int i = 0; i < drawables.size(); i++)
+				drawables.get(i).reshape(x, y, (int) viewWidth,
+						(int) viewHeight);
+		}
+	}
+
+	public void render(GL2 gl, int draw_mode, Drawable thing)
+	{
+		gl.glColor3f(1, 1, 1);
+		gl.glEnableClientState(GL2.GL_VERTEX_ARRAY);
+		gl.glVertexPointer(3, GL2.GL_FLOAT, 0, thing.getGeometry());
+		gl.glDrawArrays(draw_mode, 0, thing.getNumPoints());
+	}
+
+	public void screenToViewCoords(float[] xy)
+	{
+		float viewX = (xy[0] / screenWidth) * viewWidth - viewWidth / 2;
+		float viewY = -(xy[1] / screenHeight) * viewHeight + viewHeight / 2;
+		xy[0] = viewX;
+		xy[1] = viewY;
+	}
+
+	@Override
+	public void dispose(GLAutoDrawable drawable)
+	{
+
+	}
+
+	@Override
+	public long getTimeSinceLastDraw()
+	{
+		return timeSinceLastDraw;
+	}
+	
+	public float getViewWidth()
+	{
+		return viewWidth;
+	}
+	
+	public float getViewHeight()
+	{
+		return viewHeight;
+	}
+}
---
+++ src/PooMonkeysEngine.java
@@ -0,0 +1,263 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+import java.io.IOException;
+import java.util.ArrayList;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+
+import poomonkeys.common.AimingHUD;
+import poomonkeys.common.ExplosionController;
+import poomonkeys.common.GLClickEvent;
+import poomonkeys.common.GLClickListener;
+import poomonkeys.common.Player;
+import poomonkeys.common.Point2D;
+import poomonkeys.common.Shot;
+import poomonkeys.common.SocketListener;
+import poomonkeys.common.SocketUtil;
+import poomonkeys.common.Terrain;
+import poomonkeys.common.TerrainGenerator;
+
+public class PooMonkeysEngine implements WindowListener, MouseListener, MouseMotionListener, ActionListener, SocketListener, GLClickListener
+{
+
+	static final int STATE_CHOOSE_ANGLE = 0;
+	static final int STATE_FIRING_SHOT = 1;
+	static final int STATE_ENEMY_FIRING_SHOT = 2;
+	static final int STATE_TESTING = 3;
+	
+	public ArrayList<Player> players = new ArrayList<Player>();
+	int gameState = STATE_CHOOSE_ANGLE;
+	public int currentPlayer = 0;
+	AimingHUD angleHUD;
+	ArrayList<Shot> shots = new ArrayList<Shot>();
+	public Point2D gravity = new Point2D(0, -.0003f);
+	
+	JMenuBar menuBar = new JMenuBar();
+	JMenu menu;
+	JMenuItem hostAGame, connectToAGame;
+
+	GLRenderer renderer = null;
+	JFrame the_frame;
+	
+	Terrain the_terrain;
+	
+	static PooMonkeysEngine engine = null;
+	
+	public static void main(String[] args) 
+    {
+		PooMonkeysEngine.getInstance();
+    }
+	
+	public static PooMonkeysEngine getInstance()
+	{
+		if(engine == null) engine = new PooMonkeysEngine();
+		return engine;
+	}
+
+	public PooMonkeysEngine()
+	{
+		renderer = GLRenderer.getInstance();
+		renderer.addMouseListener(this);
+		renderer.addMouseMotionListener(this);
+		
+	    the_frame = new JFrame("Hello World");
+	    
+	    menu = new JMenu("Game");
+	    menuBar.add(menu);
+	    hostAGame = new JMenuItem("Host a game");
+	    hostAGame.setName("host");
+	    hostAGame.addActionListener(this);
+	    menu.add(hostAGame);
+	    connectToAGame = new JMenuItem("Connect to a Game");
+	    connectToAGame.setName("connect");
+	    connectToAGame.addActionListener(this);
+	    menu.add(connectToAGame);
+	    the_frame.setJMenuBar(menuBar);
+	    
+	    the_frame.getContentPane().add(renderer);
+	
+	    // shutdown the program on windows close event
+	    the_frame.addWindowListener(this);
+	
+	    the_frame.setSize(the_frame.getContentPane().getPreferredSize());
+	    the_frame.setVisible(true);
+	    
+	    players.add(new Player());
+		renderer.registerDrawable(players.get(0).tank);
+	    
+	    angleHUD = AimingHUD.getInstance();
+	    angleHUD.startButton.addGLClickListener(this);
+	    //renderer.registerDrawable(angleHUD);
+	    
+	    the_terrain = new Terrain();
+	    TerrainGenerator.generate(the_terrain);
+	    
+	    renderer.registerDrawable(the_terrain);
+	    
+	    gameState = STATE_TESTING;
+	}
+	
+	public void delete()
+	{
+		angleHUD.delete();
+		PooMonkeysEngine.engine = null;
+	}
+	
+	public void setCurrentTankAngle(float rotation)
+	{
+		players.get(currentPlayer).setAngle(rotation);
+	}
+
+	public void fireShot() 
+	{
+		GLRenderer renderer = GLRenderer.getInstance();
+		gameState = STATE_FIRING_SHOT;
+		renderer.unregisterDrawable(angleHUD);
+		Shot shot = new Shot(players.get(currentPlayer), 0, angleHUD.getPower(), gravity.x, gravity.y, renderer.viewWidth, renderer.viewHeight);
+		players.get(currentPlayer).fireShot(shot);
+		renderer.registerDrawable(shot);
+		shots.add(shot);
+	}
+
+	public void enemyFiredShot(int enemyID, float x, float y, float vx, float vy) 
+	{
+		GLRenderer renderer = GLRenderer.getInstance();
+		gameState = STATE_ENEMY_FIRING_SHOT;
+		Shot shot = new Shot(players.get(enemyID), 0, x, y, vx, vy, gravity.x, gravity.y, renderer.viewWidth, renderer.viewHeight);
+		players.get(enemyID).fireShot(shot);
+		renderer.registerDrawable(shot);
+		shots.add(shot);
+	}
+	
+	@Override
+	public void windowClosing(WindowEvent arg0) 
+	{
+		System.exit(0);
+	}
+	
+	@Override
+	public void mouseDragged(MouseEvent e) 
+	{
+		float x = e.getX();
+        float y = e.getY();
+        float real_xy[] = {x, y};
+        GLRenderer renderer = GLRenderer.getInstance();
+		renderer.screenToViewCoords(real_xy);
+		if(gameState == STATE_CHOOSE_ANGLE) {
+			angleHUD.touch(real_xy[0], real_xy[1], renderer.viewWidth, renderer.viewHeight);
+			setCurrentTankAngle(angleHUD.anglePicker.line.getRotation());
+		}
+	}
+	
+
+	@Override
+	public void mouseReleased(MouseEvent e) 
+	{
+		float x = e.getX();
+        float y = e.getY();
+		float real_xy[] = {x, y};
+		GLRenderer renderer = GLRenderer.getInstance();
+		renderer.screenToViewCoords(real_xy);
+		switch(gameState)
+		{
+			case STATE_CHOOSE_ANGLE:
+				angleHUD.click(real_xy[0], real_xy[1], renderer.viewWidth, renderer.viewHeight);
+				break;
+			case STATE_TESTING:
+				ExplosionController.explode(the_terrain, real_xy[0], real_xy[1], 2, renderer);
+				break;
+		}
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) 
+	{
+	}
+	
+	@Override
+	public void mouseMoved(MouseEvent arg0){}
+	@Override
+	public void mouseEntered(MouseEvent arg0) {}
+	@Override
+	public void mouseExited(MouseEvent arg0) {}
+	@Override
+	public void mousePressed(MouseEvent arg0) {}
+	@Override
+	public void windowActivated(WindowEvent arg0) {}
+	@Override
+	public void windowClosed(WindowEvent arg0) {}
+	@Override
+	public void windowDeactivated(WindowEvent arg0) {}
+	@Override
+	public void windowDeiconified(WindowEvent arg0) {}
+	@Override
+	public void windowIconified(WindowEvent arg0) {}
+	@Override
+	public void windowOpened(WindowEvent arg0) {}
+
+	@Override
+	public void actionPerformed(ActionEvent e) 
+	{
+		String action = ((JMenuItem)e.getSource()).getName();
+		
+		if(action.equals("host")) 
+		{
+			try 
+			{
+				SocketUtil.hostGame();
+			} catch (IOException e1) {}
+		}
+		else if(action.equals("connect"))
+		{
+			String ip = (String)JOptionPane.showInputDialog(
+					the_frame,
+                    "Enter the IP to connect to",
+                    "Direct Connect",
+                    JOptionPane.QUESTION_MESSAGE);
+
+			if ((ip != null) && (ip.length() > 0)) 
+			{
+				SocketUtil.connectToIP(ip);
+			}
+		}
+	}
+	
+	@Override
+	public void playerJoined() 
+	{
+		players.add(new Player());
+	}
+
+	@Override
+	public void incomingMessage(int socket_id, String[] data) 
+	{
+		// Player id (first argument) is always one more than socket id, this 
+		// is because the first player's id is 0 and the first player has no 
+		// socket. Then as each other player connects they are given a socket_id
+		// and then immediately added to the player list, so the relationship
+		// should always hold. The same is true for removing players (I think).
+		engine.enemyFiredShot(socket_id+1, 
+								Float.parseFloat(data[0]), 
+								Float.parseFloat(data[1]), 
+								Float.parseFloat(data[2]), 
+								Float.parseFloat(data[3]));
+	}
+
+	@Override
+	public void glClicked(GLClickEvent evt) 
+	{
+		if(evt.getSource() == angleHUD.startButton)
+		{
+			fireShot();
+		}
+	}
+
+}
